52599ee822c3fe5a112cabeb5c03269bcb7c46fc
MRealFB <mrealfb@gmail.com>
2025-07-09 15:10:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -121,4 +121,7 @@ def export():
     }
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
+
